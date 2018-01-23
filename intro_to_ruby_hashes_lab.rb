6fe2f445d792b98469ad47c21a3317945e0a1ecb
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 04:14:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,13 +5,28 @@ def new_hash
 end
 
 def actor
-  hash = {:name => "Dwayne The Rock Johnson"}
+  hash = {
+    :name => "Dwayne The Rock Johnson"
+  }
 end
 
 def monopoly
   hash = {
     :railroads => {
+    }
+  }
+end
 
+def monopoly_with_second_tier
+  hash = {
+    :railroads => {
+      :pieces => 4,
+      :names => {
+
+      }
+      :rent_in_dollars => {
+
+      }
     }
   }
 end
